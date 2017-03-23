3928ee53ac18b38b0ac4932e01137a92a4352846
InsertioMacBookPro <insertioLab@Insertios-MacBook-Pro.local>
2017-03-23 16:55:00 -0400

    work and file updates

--- node_mboard/node_mboard.ino
+++ node_mboard/node_mboard.ino
@@ -6,6 +6,11 @@
 #include <Servo.h>
 #include <Button.h>
 
+#define DC       0
+#define SERVO    1
+#define SOLENOID 2
+
+
 /*
   This example is for Series 2 XBee
   Receives a ZB RX packet and sets a PWM value based on packet data.
@@ -18,24 +23,13 @@ XBeeResponse response = XBeeResponse();
 ZBRxResponse rx = ZBRxResponse();
 ModemStatusResponse msr = ModemStatusResponse();
 
+Servo servoA;
+Servo servoB;
 
-const int buttonPin = A0;   // the number of the pushbutton pin
-const int sensorPin = A4;
-const int ledPin =  5;      // the number of the LED pin
+Button switch_servo = Button(5, BUTTON_PULLUP_INTERNAL);
+Button switch_solenoide = Button(9, BUTTON_PULLUP_INTERNAL);
 
-//Zigbee Transmit Request API packet
-ZBTxRequest txRequest;
-uint8_t payload[] = {
-  0
-};
-
-
-int lastSend = 0;
-
-int dataIn = 0;
-
-// variable for slow transitions
-float outputValue = 0.0;
+int actuator = DC;
 
 
 void setup() {
@@ -47,99 +41,53 @@ void setup() {
   Serial.begin(57600);
 
   Serial1.begin(57600);
-  //xbee.begin(Serial1);
-  xbee.setSerial(Serial1);
+  xbee.begin(Serial1);
 
-  // Prepare the Zigbee Transmit Request API packet
-  // Set the paylod for the data to be sent
-  txRequest.setPayload(payload, sizeof(payload));
-  // Identifies the UART data frame for the host to correlate with a
-  // subsequent ACK (acknowledgment). If set to 0, no response is sent.
-  txRequest.setFrameId(0);
-  // Disable ACK (acknowledgement)
-  txRequest.setOption(1);
+  //Switch D5
 
-  pinMode(buttonPin, INPUT);
-  pinMode(ledPin, OUTPUT);
+  if (switch_servo.isPressed()) {
+    actuator = SERVO;
+
+    servoA.attach(A5);
+    servoB.attach(A4);
+
+    //Switch D9
+  } else if (switch_solenoide.isPressed()) {
+    actuator = SOLENOID;
+
+    //Motor 1 direction - OUT1 & OUT2
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
+    //Motor 1 Speed
+    pinMode(10, OUTPUT);
+
+    //Motor 2 direction - OUT3 & OUT4
+    pinMode(12, OUTPUT);
+    pinMode(13, OUTPUT);
+    //Motor 2 Speed
+    pinMode(11, OUTPUT);
+
+  } else {
+    actuator = DC;
+
+    //Motor 1 direction - OUT1 & OUT2
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
+    //Motor 1 Speed
+    pinMode(10, OUTPUT);
+
+    //Motor 2 direction - OUT3 & OUT4
+    pinMode(12, OUTPUT);
+    pinMode(13, OUTPUT);
+    //Motor 2 Speed
+    pinMode(11, OUTPUT);
+
+  }
 }
 
 
 // continuously reads packets, looking for ZB Receive or Modem Status
 void loop() {
-  int buttonValue = digitalRead(buttonPin);
-  int sensorValue = analogRead(sensorPin) / 4;
-
-
-
-
-
-
-  int desiredValue = 50;
-
-  // if someone is detected
-  if (sensorValue > 120) {
-    // set the output value to the maximum
-    desiredValue = 255;
-
-    // smoothly transition towards the max value
-    float dif = desiredValue - outputValue;
-    if (abs(dif) > 1.0) {
-      outputValue = outputValue + dif / 8.0;
-    }
-  } else {
-    // set the output value to the maximum
-    desiredValue = 50;
-
-    // smoothly transition towards the max value
-    float dif = desiredValue - outputValue;
-    if (abs(dif) > 1.0) {
-      outputValue = outputValue + dif / 64.0;
-    }
-  }
-
-  // change the analog out value:
-  analogWrite(ledPin, outputValue);
-  // print out the value you read:
-  Serial.println(outputValue);
-  delay(30);        // delay in between reads for stability
-
-
-
-
-
-
-
-  //if (abs(lastSend - sensorValue) > 30) {
-  //  sendPacket(XBeeAddress64(0x00000000, 0x00000000), sensorValue);
-  //sendPacket(XBeeAddress64(0x0013a200, 0x40e66dcd), sensorValue);
-  //  lastSend = sensorValue;
-
-  //  Serial.println(sensorValue);
-  //}
-
-  //if (buttonValue == HIGH)
-  //  digitalWrite(ledPin, HIGH);
-  //else
-  //  digitalWrite(ledPin, LOW);
-
-  if (abs(lastSend - buttonValue) > 0) {
-    // to send only to the coordinator
-    //sendPacket(XBeeAddress64(0x00000000, 0x00000000), buttonValue);
-    // to broadcast the message
-    //sendPacket(XBeeAddress64(0x00000000, 0x0000ffff), buttonValue);
-    // to send to specific XBee
-    sendPacket(XBeeAddress64(0x0013a200, 0x40e66da0), buttonValue);
-    sendPacket(XBeeAddress64(0x0013a200, 0x40e66dcd), buttonValue);
-    sendPacket(XBeeAddress64(0x0013a200, 0x40e66c35), buttonValue);
-    sendPacket(XBeeAddress64(0x0013a200, 0x40e66dca), buttonValue);
-    sendPacket(XBeeAddress64(0x0013a200, 0x40e66c1b), buttonValue);
-
-    lastSend = buttonValue;
-
-    Serial.println(buttonValue);
-  }
-
-
   xbee.readPacket();
 
   if (xbee.getResponse().isAvailable()) {
@@ -151,26 +99,82 @@ void loop() {
       xbee.getResponse().getZBRxResponse(rx);
 
       // get value of the first byte in the data
-      dataIn = rx.getData(0);
+      int data = rx.getData(0);
+      
+      Serial.println(data);
 
-      digitalWrite(ledPin, dataIn);
-      Serial.println(dataIn);
+      // Actuator Servo
+      if (actuator == SERVO ) {
+        data = map(data, 0, 255, 0, 180);
 
+        servoA.write(data);
+        servoB.write(data);
+
+        // Actuator Solenoide - Switch D9
+      } else if (actuator == SOLENOID) {
+
+        digitalWrite(7, HIGH);
+        digitalWrite(8, LOW);
+
+        digitalWrite(12, HIGH);
+        digitalWrite(13, LOW);
+
+        digitalWrite(10, HIGH);
+        digitalWrite(11, HIGH);
+        delay (map(data, 0, 255, 1, 25));
+        digitalWrite(10, LOW);
+        digitalWrite(11, LOW);
+
+
+
+      } else if (actuator == DC) {
+        data = map(data, 0, 255, 0, 255);
+
+
+#ifdef BIDIRECTIONNAL_DC
+
+        // TWO DIRECTIONAL DC MOTOR - OPTIONAL
+
+        if (data < 128) {
+          digitalWrite(7, LOW);
+          digitalWrite(8, HIGH);
+
+          analogWrite(10, 255 - map(data, 0, 127, 0, 255));
+
+          digitalWrite(12, LOW);
+          digitalWrite(13, HIGH);
+
+          analogWrite(11, 255 - map(data, 0, 127, 0, 255));
+
+        } else {
+          digitalWrite(7, HIGH);
+          digitalWrite(8, LOW);
+
+          analogWrite(10, map(data, 128, 255, 0, 255));
+
+          digitalWrite(12, HIGH);
+          digitalWrite(13, LOW);
+
+          analogWrite(11, map(data, 128, 255, 0, 255));
+        }
+
+
+#else
+
+        // ONE DIRECTION DC MOTOR
+
+        digitalWrite(7, HIGH);
+        digitalWrite(8, LOW);
+
+        analogWrite(10, data);
+
+        digitalWrite(12, HIGH);
+        digitalWrite(13, LOW);
+
+        analogWrite(11, data);
+#endif
+      }
     }
   }
-
-  //if (dataIn == HIGH)
-  //  digitalWrite(ledPin, HIGH);
-  //else
-  //  digitalWrite(ledPin, LOW);
-
-}
-
-void sendPacket(XBeeAddress64 addr64, uint8_t val) {
-  // Set the destination address of the message
-  txRequest.setAddress64(addr64);
-  payload[0] = val;
-  // Send the message
-  xbee.send(txRequest);
 }
 
