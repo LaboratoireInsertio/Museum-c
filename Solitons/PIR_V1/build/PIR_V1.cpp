e9fecee2a3e09ea0ef440ac2b3abba24c747ea8e
InsertioLab <james.partaik@uqac.ca>
2017-04-03 21:47:52 -0400

    Update codes solitons

---
+++ Solitons/PIR_V1/build/PIR_V1.cpp
@@ -0,0 +1,24 @@
+#include "WProgram.h"
+void setup();
+void loop();
+int speed = 128;
+
+void setup()
+{
+  pinMode(4, OUTPUT);
+  pinMode(5, INPUT);
+}
+
+void loop()
+{
+  if(digitalRead(5) == 1) {
+    speed = random(0, 255);
+    delay(random(0, 1500));
+  } 
+  else {
+    speed = 0;
+  }
+  analogWrite(4, speed);
+}
+
+
